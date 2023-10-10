ff375f6a39546cc79a5d9fdec233cade0e599b4a
Nathan Walker <walkerrunpdx@gmail.com>
2023-10-09 21:14:38 -0700

    chore: vue

--- apps/nativescript-vue/src/components/Home.vue
+++ apps/nativescript-vue/src/components/Home.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
+import { EventData } from '@nativescript/core';
 import { setupSwiftUI, viewBindings } from '@vision/nativescript-data';
 setupSwiftUI();
 const ui = viewBindings;
+function openDetail(args: EventData) {}
 </script>
 
 <template>
@@ -39,25 +41,25 @@ const ui = viewBindings;
           rows="auto,50"
           columns="auto,30,auto,30,auto"
           class="align-bottom h-center mt-2"
-          (loaded)="ui.loadedTOC($event)"
+          @loaded="ui.loadedTOC($event)"
         >
           <StackLayout
             id="globe"
             col="0"
             width="330"
-            (tap)="openDetail()"
+            @tap="openDetail"
             class="p-4"
           >
             <Label
-              [text]="ui.data.eyebrow('globe')"
+              :text="ui.data.eyebrow('globe')"
               class="text-[17px] font-semibold text-white/50"
             ></Label>
             <Label
-              [text]="ui.data.heading('globe')"
+              :text="ui.data.heading('globe')"
               class="text-[30px] font-bold mt-1"
             ></Label>
             <Label
-              [text]="ui.data.abstract('globe')"
+              :text="ui.data.abstract('globe')"
               class="text-[18px] font-semibold mt-2"
               textWrap="true"
             ></Label>
@@ -67,19 +69,19 @@ const ui = viewBindings;
             id="orbit"
             col="2"
             width="330"
-            (tap)="openDetail()"
+            @tap="openDetail"
             class="p-4"
           >
             <Label
-              [text]="ui.data.eyebrow('orbit')"
+              :text="ui.data.eyebrow('orbit')"
               class="text-[17px] font-semibold text-white/50"
             ></Label>
             <Label
-              [text]="ui.data.heading('orbit')"
+              :text="ui.data.heading('orbit')"
               class="text-[30px] font-bold mt-1"
             ></Label>
             <Label
-              [text]="ui.data.abstract('orbit')"
+              :text="ui.data.abstract('orbit')"
               class="text-[18px] font-semibold mt-2"
               textWrap="true"
             ></Label>
@@ -89,19 +91,19 @@ const ui = viewBindings;
             id="solar"
             col="4"
             width="330"
-            (tap)="openDetail()"
+            @tap="openDetail"
             class="p-4"
           >
             <Label
-              [text]="ui.data.eyebrow('solar')"
+              :text="ui.data.eyebrow('solar')"
               class="text-[17px] font-semibold text-white/50"
             ></Label>
             <Label
-              [text]="ui.data.heading('solar')"
+              :text="ui.data.heading('solar')"
               class="text-[30px] font-bold mt-1"
             ></Label>
             <Label
-              [text]="ui.data.abstract('solar')"
+              :text="ui.data.abstract('solar')"
               class="text-[18px] font-semibold mt-2"
               textWrap="true"
             ></Label>
